8412db8b2e470fe29ecdc1111cea86613b4d6014
lhnhi420@gmail.com <lhnhi420@gmail.com>
2025-01-12 00:39:12 +0700

    dung khung layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,12 +1,18 @@
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
         <div>
             <Header></Header>
-            <Sidebar></Sidebar>
-            <div className="content">{children}</div>
+            <div className={cx('container')}>
+                <Sidebar></Sidebar>
+                <div className={cx('content')}>{children}</div>
+            </div>
         </div>
     );
 }
